1482757a5a79f3f3d5304cbfdc732060aac57690
Per Wagner Nielsen <perwagnernielsen@gmail.com>
2020-02-08 13:36:03 +0800

    Part 2 templates

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from sanic import Sanic
 from sanic import response
+from sanic_jinja2 import SanicJinja2
 
-app = Sanic(name='My Sanic Web App')
+app = Sanic(name='Sanic Web Tutorial')
+jinja = SanicJinja2(app, pkg_name="main")
 
 
+@app.route("/")
 async def index(request):
-    return response.html("<h1>Hello World</h1>")
-
-
-app.add_route(index, "/")
-app.add_route(index, "/main")
+    context = {
+        "title": "Sanic Web Title",
+        "desc": "Something else here",
+    }
+    return jinja.render("index.html", request, context=context)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
